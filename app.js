e1b8461b1611f0b83a480e037f7fc656e1a7416e
Ricardoguima <125500278+Ricardoguima@users.noreply.github.com>
2025-04-15 17:40:16 -0700

    fixed route for browser hit

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log("Server Listening on PORT:", PORT);
-  });
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server Listening on PORT:", PORT);
+});
 
-  app.get("/status", (request, response) => {
-    const status = {
-        "Status": "Running"
-    };
-    
-    response.send(status)
-  });
+app.get("/", (req, res) => {
+  res.send("OHHHH YEAHH it totally works");
+});
