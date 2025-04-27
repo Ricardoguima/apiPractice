babd4703c06f4aff96a1754e55fc780d06e97ccc
Ricardoguima <125500278+Ricardoguima@users.noreply.github.com>
2025-04-27 13:43:06 -0700

    Changed style of message

--- app.js
+++ app.js
@@ -10,5 +10,31 @@ app.listen(PORT, "0.0.0.0", () => {
 });
 
 app.get("/", (req, res) => {
-  res.send("OHHHH YEAHH it totally works");
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>DevOps Demo</title>
+        <style>
+          body {
+            background-color: #111;
+            color: #ffffff;
+            font-family: monospace;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+            font-size: 2rem;
+            text-align: center;
+          }
+        </style>
+      </head>
+      <body>
+        OHHHHH YEAHHHH
+        <br><br>
+        ...it totally works 
+      </body>
+    </html>
+  `);
 });
